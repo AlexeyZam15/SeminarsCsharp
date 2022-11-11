3dc664eaaf742dc8aef14d442480f0d4f186f158
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-11-11 19:07:09 +1000

    Fixed some problems

--- task3/Program.cs
+++ task3/Program.cs
@@ -4,13 +4,12 @@
 // 5 -> Пятница
 Console.Clear();
 Console.WriteLine("Введите номер дня недели");
-int day = Convert.ToInt32(Console.ReadLine());
-Console.Write("День недели ");
-if (day == 1) Console.WriteLine("Понедельник");
-else if (day == 2) Console.WriteLine("Вторник");
-else if (day == 3) Console.WriteLine("Среда");
-else if (day == 4) Console.WriteLine("Четверг");
-else if (day == 5) Console.WriteLine("Пятница");
-else if (day == 6) Console.WriteLine("Суббота");
-else if (day == 7) Console.WriteLine("Воскресенье");
+string day = Console.ReadLine();
+if (day == "1") Console.WriteLine("Понедельник");
+else if (day == "2") Console.WriteLine("Вторник");
+else if (day == "3") Console.WriteLine("Среда");
+else if (day == "4") Console.WriteLine("Четверг");
+else if (day == "5") Console.WriteLine("Пятница");
+else if (day == "6") Console.WriteLine("Суббота");
+else if (day == "7") Console.WriteLine("Воскресенье");
 else Console.WriteLine("Такого дня недели нет");
