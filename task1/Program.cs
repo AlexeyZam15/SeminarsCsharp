e564562fa2c6f612c9aa46afd2da7b16dd7994f9
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-11-11 18:38:51 +1000

    Solved task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,7 +6,15 @@
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
 
-Console.WriteLine("Введите два целых числа");
+Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
-
+if (a == b*b) 
+{
+    Console.WriteLine($"Число {a} является квадратом {b}");
+}
+else 
+{
+    Console.WriteLine($"Число {a} не является квадратом {b}");
+}
