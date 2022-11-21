84173ef091594778e468e270612b2b21a0485624
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-11-21 20:23:36 +1000

    changed task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -13,17 +13,39 @@ while (number < 10000 || number > 99999)
     if (number < 0 || number < 10000 || number > 99999) Console.WriteLine("Введены неверные данные");
 }
 
-int Reverse(int numb)
+// int Reverse(int numb)
+// {
+//     int firstDigit = numb / 10000;
+//     int secondDigit = numb % 10000 / 1000;
+//     int thirdDigit = numb % 1000 / 100;
+//     int fourthDigit = numb % 100 / 10;
+//     int fifthDigit = numb % 10;
+//     return fifthDigit * 10000 + fourthDigit * 1000 + thirdDigit * 100 + secondDigit * 10 + firstDigit;
+// }
+
+int Reverse(int numb, int digits)
 {
-    int firstDigit = numb / 10000;
-    int secondDigit = numb % 10000 / 1000;
-    int thirdDigit = numb % 1000 / 100;
-    int fourthDigit = numb % 100 / 10;
-    int fifthDigit = numb % 10;
-    return fifthDigit * 10000 + fourthDigit * 1000 + thirdDigit * 100 + secondDigit * 10 + firstDigit;
+    int result = 0;
+    int length = 1;
+    for (int i = 1; i < digits; i++)
+    {
+        length = length * 10;
+    }
+    for (int j = 1; j <= length; j = j * 10)
+    {
+        result = result + numb / (length / j) * j;
+        numb = numb % (length / j);
+    }
+    // for (int j = 10000; j != 1; j = j / 10) // 12345
+    // {
+    //     result = result + numb % 10 * j; // 50000 // 4000 // 300
+    //     numb = numb / 10; // 1234 // 123 //
+    // }
+    // result = result + numb;
+    return result;
 }
 
-int reverse = Reverse(number);
+int reverse = Reverse(number, 5);
 Console.WriteLine($"Обратное число: {reverse}");
 Console.WriteLine("Является ли число палиндромом?");
 Console.WriteLine(number == reverse ? "да" : "нет");
