f2a0e1739760ba7a87cc20417fab3c2cad0211e3
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-11-30 19:05:45 +1000

    changed task43

--- task43/Program.cs
+++ task43/Program.cs
@@ -2,29 +2,40 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-double[] StringToNumbersArray(string str, int amountNumbers)
+// double[] StringToNumbersArray(string str, int amountNumbers)
+// {
+//     double[] arr = new double[amountNumbers];
+//     string number = string.Empty;
+//     int indexArray = 0;
+//     for (int i = 0; i < str.Length; i++)
+//     {
+//         if (str[i] != ',')
+//         {
+//             number += str[i];
+//         }
+//         if (str[i] == ',')
+//         {
+//             arr[indexArray] = Convert.ToDouble(number);
+//             number = string.Empty;
+//             indexArray++;
+//         }
+//         if (i == str.Length - 1)
+//         {
+//             arr[indexArray] = Convert.ToDouble(number);
+//             number = string.Empty;
+//             indexArray++;
+//         }
+//     }
+//     return arr;
+// }
+
+double[] StringToNumbersArray(string str, string separator)
 {
-    double[] arr = new double[amountNumbers];
-    string number = string.Empty;
-    int indexArray = 0;
-    for (int i = 0; i < str.Length; i++)
+    string[] arrString = str.Split($"{separator}");
+    double[] arr = new double[arrString.Length];
+    for (int i = 0; i < arr.Length; i++)
     {
-        if (str[i] != ',')
-        {
-            number += str[i];
-        }
-        if (str[i] == ',')
-        {
-            arr[indexArray] = Convert.ToDouble(number);
-            number = string.Empty;
-            indexArray++;
-        }
-        if (i == str.Length - 1)
-        {
-            arr[indexArray] = Convert.ToDouble(number);
-            number = string.Empty;
-            indexArray++;
-        }
+        arr[i] = Convert.ToDouble(arrString[i]);
     }
     return arr;
 }
@@ -44,8 +55,14 @@ double EquationY(double b1, double k1, double b2, double k2)
 Console.Write($"Введите значения b1, k1, b2 и k2 через запятую: ");
 string numbersString = Console.ReadLine();
 
-double[] array = StringToNumbersArray(numbersString, 4);
+double[] array = StringToNumbersArray(numbersString, ",");
 
-double equationX = Math.Round(EquationX(array[0], array[1], array[2], array[3]),1);
-double equationY = Math.Round(EquationY(array[0], array[1], array[2], array[3]),1);
-Console.WriteLine($"b1 = {array[0]}, k1 = {array[1]}, b2 = {array[2]}, k2 = {array[3]} -> ({equationX}; {equationY})");
+double b1 = array[0];
+double k1 = array[1];
+double b2 = array[2];
+double k2 = array[3];
+
+double equationX = Math.Round(EquationX(b1, k1, b2, k2), 1);
+double equationY = Math.Round(EquationY(b1, k1, b2, k2), 1);
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({equationX}; {equationY})");
