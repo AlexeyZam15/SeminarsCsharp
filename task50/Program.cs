00389a97bb604355ccc98baa9eda8b52647bb88b
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-12-02 22:08:58 +1000

    add task51

---
+++ task50/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 50. Напишите программу, которая 
+// на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+            // TODO: вывести индексы массива.
+            // matrix[i, j] = i * 10 + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string beginRow, string separatorElems, string endRow)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write(beginRow);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],4}{separatorElems}");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(endRow);
+    }
+}
+
+bool IndexMatrix(int[,] matrix, int rowIndex1, int columnIndex1)
+{
+    if (rowIndex1 < 0 || columnIndex1 < 0) return false;
+    if (rowIndex1 >= matrix.GetLength(0) || columnIndex1 >= matrix.GetLength(1)) return false;
+    return true;
+}
+
+void StringToTwoNumbers(string str, out int rowIndex1, out int columnIndex1)
+{
+    string[] arrString = str.Split(',');
+    (int, int) numbers = (0, 0);
+    rowIndex1 = Convert.ToInt32(arrString[0]);
+    columnIndex1 = Convert.ToInt32(arrString[1]);
+}
+
+// Console.Write("Введите индекс строки: ");
+// int rowIndex = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите индекс столбца: ");
+// int columnIndex = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array, "", "", "");
+
+Console.Write("Введите индексы строки и столбца через запятую: ");
+string numbersString = Console.ReadLine();
+
+int rowIndex, columnIndex;
+StringToTwoNumbers(numbersString, out rowIndex, out columnIndex);
+
+Console.WriteLine(IndexMatrix(array, rowIndex, columnIndex) ? $"Элемент массива [{rowIndex},{columnIndex}] = {array[rowIndex, columnIndex]}"
+                                                            : "Такого числа в массиве нет");
