7a0b5652ce8f2d3a8d3c9f4c97b77b46797da698
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-12-14 21:58:55 +1000

    changed task62

--- task62/Program.cs
+++ task62/Program.cs
@@ -9,61 +9,64 @@ int[,] CreateMatrixIncIntSpiral(int rows, int columns, int startNumber)
 {
     int[,] matrix = new int[rows, columns];
     int direction = 0;
-    int size = rows * columns;
+    int size = matrix.Length;
     int indexi = 0;
     int indexj = 0;
     int directionLength = columns;
-    while (startNumber <= size)
+    int count = 0;
+    while (count < size)
     {
-        for (int j = 0; j < directionLength; j++)
         {
-            // Console.WriteLine($"{direction} {directionLength} {indexi} {indexj} {startNumber + 1}");
+            for (int j = 0; j < directionLength; j++, count++)
+            {
+                // Console.WriteLine($"{direction} {directionLength} {indexi} {indexj} {startNumber + 1}");
+                if (direction == 0)
+                {
+                    matrix[indexi, indexj++] = startNumber++;
+                }
+                else if (direction == 1)
+                {
+                    matrix[indexi++, indexj] = startNumber++;
+                }
+                else if (direction == 2)
+                {
+                    matrix[indexi, indexj--] = startNumber++;
+                }
+                else if (direction == 3)
+                    matrix[indexi--, indexj] = startNumber++;
+            }
             if (direction == 0)
             {
-                matrix[indexi, indexj++] = startNumber++;
+                rows--;
+                directionLength = rows;
+                indexj--;
+                indexi++;
+                direction = 1;
             }
             else if (direction == 1)
             {
-                matrix[indexi++, indexj] = startNumber++;
+                columns--;
+                directionLength = columns;
+                direction = 2;
+                indexi--;
+                indexj--;
             }
             else if (direction == 2)
             {
-                matrix[indexi, indexj--] = startNumber++;
+                rows--;
+                directionLength = rows;
+                direction = 3;
+                indexj++;
+                indexi--;
             }
             else if (direction == 3)
-                matrix[indexi--, indexj] = startNumber++;
-        }
-        if (direction == 0)
-        {
-            rows--;
-            directionLength = rows;
-            indexj--;
-            indexi++;
-            direction = 1;
-        }
-        else if (direction == 1)
-        {
-            columns--;
-            directionLength = columns;
-            direction = 2;
-            indexi--;
-            indexj--;
-        }
-        else if (direction == 2)
-        {
-            rows--;
-            directionLength = rows;
-            direction = 3;
-            indexj++;
-            indexi--;
-        }
-        else if (direction == 3)
-        {
-            columns--;
-            directionLength = columns;
-            direction = 0;
-            indexi++;
-            indexj++;
+            {
+                columns--;
+                directionLength = columns;
+                direction = 0;
+                indexi++;
+                indexj++;
+            }
         }
     }
     return matrix;
